e2dbb6fb000965d5c41534b54ba29bb9eb66634b
Bankkittirat <banklovecom1669@gmail.com>
2020-12-26 19:29:17 -0800

    first commit

---
+++ lib/models/book.dart
@@ -0,0 +1,230 @@
+import 'package:meta/meta.dart';
+
+BookList booklist = BookList(books: [
+  Book(
+      title: "Harry Potter and the Deathly Hallows",
+      cover:
+          "https://upload.wikimedia.org/wikipedia/en/a/a9/Harry_Potter_and_the_Deathly_Hallows.jpg",
+      age: "ALL AGE",
+      author: "J. K. Rowling",
+      authorImg:
+          "https://images.gr-assets.com/authors/1510435123p5/1077326.jpg",
+      franchise: true,
+      genre: "Fantasy",
+      lanugage: "ENGLISH",
+      rating: 4.5,
+      price: 13.49,
+      summary:
+          "แฮร์รี่ (แดเนียลแรดคลิฟฟ์), รอน (รูเพิร์ตกรินต์) และเฮอร์ไมโอนี (เอ็มม่าวัตสัน) ไม่ได้รับคำแนะนำและคุ้มครองจากอาจารย์ของพวกเขาโดยปราศจากคำแนะนำและความคุ้มครองจากอาจารย์ของพวกเขาแฮร์รี่ (แดเนียลแรดคลิฟฟ์) รอน (รูเพิร์ตกรินท์) และเฮอร์ไมโอนี่ (เอ็มม่าวัตสัน) เริ่มภารกิจเพื่อทำลายฮอร์ครักซ์ซึ่งเป็นแหล่งที่มาของความเป็นอมตะของโวลเดอมอร์ แม้ว่าพวกเขาจะต้องพึ่งพากันและกันมากขึ้นกว่าเดิม แต่พลังแห่งความมืดก็ขู่ว่าจะฉีกพวกเขาออกจากกัน ผู้เสพความตายของโวลเดอมอร์ได้ยึดการควบคุมของกระทรวงเวทมนตร์และฮอกวอตส์และพวกเขากำลังตามหาแฮร์รี่แม้ในขณะที่เขาและเพื่อน ๆ ของเขาเตรียมการประลองขั้นสูงสุด",
+      wideImage: "https://wallpaperplay.com/walls/full/9/a/9/64844.jpg"),
+
+  Book(
+      title: "A Song Of Ice And Fire",
+      cover:
+          "https://upload.wikimedia.org/wikipedia/en/thumb/d/dc/A_Song_of_Ice_and_Fire_book_collection_box_set_cover.jpg/220px-A_Song_of_Ice_and_Fire_book_collection_box_set_cover.jpg",
+      age: "ABOVE 18",
+      author: "George R. R. Martin",
+      authorImg:
+          "https://www.biography.com/.image/t_share/MTQ4NDc2MTkxNTY3NzgzMTE1/george_rr_martin_photo_mark_davis_wireimage_via_getty_images_164117282_resized.jpg",
+      franchise: true,
+      genre: "Sci-Fi",
+      lanugage: "ENGLISH",
+      price: 27.34,
+      rating: 4.4,
+      summary:
+          "Game of Thrones มีพื้นฐานมาจากโครงเรื่องของ A Song of Ice and Fire ซึ่งตั้งอยู่ใน Seven Kingdoms of Westeros และทวีป Essos ซีรีส์เล่าถึงการต่อสู้ของราชวงศ์ที่รุนแรงระหว่างตระกูลอันสูงส่งของอาณาจักรเพื่อชิงบัลลังก์เหล็กในขณะที่ครอบครัวอื่น ๆ ต่อสู้เพื่อเอกราชจากมัน Game of Thrones มีพื้นฐานมาจากโครงเรื่องของ A Song of Ice and Fire ซึ่งตั้งอยู่ใน Seven Kingdoms of Westeros และทวีป Essos ซีรีส์เล่าถึงการต่อสู้ของราชวงศ์ที่รุนแรงระหว่างตระกูลอันสูงส่งของอาณาจักรเพื่อชิงบัลลังก์เหล็กในขณะที่ครอบครัวอื่น ๆ ต่อสู้เพื่อเอกราชจากมัน",
+      wideImage:
+          "https://images-na.ssl-images-amazon.com/images/I/91KwasehsHL._RI_.jpg"),
+
+  Book(
+      title: "The Lord Of The Rings",
+      cover:
+          "https://images-na.ssl-images-amazon.com/images/I/51EstVXM1UL._SX331_BO1,204,203,200_.jpg",
+      age: "ALL AGE",
+      author: "J. R. R. Tolkien",
+      authorImg:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJA6kQWR4l52ykTTtyFuUb6WEmXixGqzU4_QWIOLS0bfIGCUMW",
+      franchise: true,
+      genre: "Sci-Fi",
+      lanugage: "ENGLISH",
+      rating: 4.0,
+      price: 11.49,
+      summary:
+          "อนาคตของอารยธรรมอยู่ในชะตากรรมของ One Ring ซึ่งสูญหายไปหลายศตวรรษ กองกำลังที่ทรงพลังไม่ลดละในการค้นหามัน แต่โชคชะตาทำให้ฮอบบิทหนุ่มนามโฟรโดแบ็กกิ้นส์ (เอไลจาห์วูด) ผู้สืบทอดแหวนและก้าวเข้าสู่ตำนาน งานที่น่ากลัวรออยู่ข้างหน้าสำหรับโฟรโดเมื่อเขากลายเป็นผู้ถือแหวน - เพื่อทำลาย One Ring ในกองไฟของภูเขาดูมที่ซึ่งมันถูกปลอมแปลง",
+      wideImage:
+          "https://4.bp.blogspot.com/-CqmWhF3fPgA/WQlvMBGFILI/AAAAAAAAAbs/brv0zDl-lFUJWhofGthQXMoKJklDsNIjQCLcB/s1600/The%2BLord%2Bof%2Bthe%2BRings.jpg"),
+
+  Book(
+      title: "Rita Hayworth and Shawshank Redemption",
+      cover:
+          "https://upload.wikimedia.org/wikipedia/en/a/a9/Harry_Potter_and_the_Deathly_Hallows.jpg",
+      age: "ABOVE 18",
+      author: "Steven King",
+      authorImg: "https://images.gr-assets.com/authors/1362814142p5/3389.jpg",
+      franchise: true,
+      genre: "Crime Fiction",
+      lanugage: "ENGLISH",
+      price: 10.87,
+      rating: 4.9,
+      summary:
+          "Andy Dufresne (Tim Robbins) ถูกตัดสินจำคุกสองชีวิตติดต่อกันในข้อหาฆาตกรรมภรรยาและคนรักของเขาและถูกตัดสินให้ติดคุกที่ยากลำบาก อย่างไรก็ตามมีเพียงแอนดี้เท่านั้นที่รู้ว่าเขาไม่ได้ก่ออาชญากรรม ระหว่างอยู่ที่นั่นเขาสร้างมิตรภาพกับเร้ด (มอร์แกนฟรีแมน) พบกับความโหดร้ายของชีวิตในคุกปรับตัวช่วยผู้คุม ฯลฯ ทั้งหมดนี้ในรอบ 19 ปี",
+      wideImage:
+          "http://images2.fanpop.com/image/photos/10300000/Shawshank-Redemption-Wallpaper-the-shawshank-redemption-10336576-1024-768.jpg"),
+
+  Book(
+      title: "The Godfather",
+      cover:
+          "https://upload.wikimedia.org/wikipedia/en/thumb/f/f4/Godfather-Novel-Cover.png/175px-Godfather-Novel-Cover.png",
+      age: "ALL AGE",
+      author: "Mario Puzo",
+      authorImg:
+          "https://upload.wikimedia.org/wikipedia/en/thumb/0/0c/Mario_Puzo.jpg/220px-Mario_Puzo.jpg",
+      franchise: true,
+      genre: "Crime Fiction",
+      lanugage: "ENGLISH",
+      price: 13.60,
+      rating: 4.8,
+      summary:
+          "ละครม็อบเรื่องนี้ได้รับการยกย่องอย่างกว้างขวางว่าเป็นหนึ่งในภาพยนตร์ที่ยิ่งใหญ่ที่สุดตลอดกาลซึ่งสร้างจากนวนิยายชื่อเดียวกันของมาริโอพูโซมุ่งเน้นไปที่ตระกูลอาชญากรรมชาวอิตาลี - อเมริกันที่ทรงพลังของ Don Vito Corleone (Marlon Brando) เมื่อลูกชายคนสุดท้องของดอนไมเคิล (อัลปาชิโน) เข้าร่วมกับกลุ่มมาเฟียโดยไม่เต็มใจเขาต้องเข้าไปพัวพันกับวงจรแห่งความรุนแรงและการทรยศที่หลีกเลี่ยงไม่ได้ แม้ว่าไมเคิลจะพยายามรักษาความสัมพันธ์แบบปกติกับภรรยาของเขาเคย์ (ไดแอนคีตัน) แต่เขาก็ดึงลึกเข้าไปในธุรกิจของครอบครัว",
+      wideImage: "https://wallpapercave.com/wp/jwavrlw.jpg"),
+//after these all are the once which are not on the top shelf
+
+  Book(
+      title: "The Da Vinci Code",
+      cover:
+          "https://upload.wikimedia.org/wikipedia/en/thumb/6/6b/DaVinciCode.jpg/220px-DaVinciCode.jpg",
+      age: "ALL AGE",
+      author: "Dan Brown",
+      authorImg:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Dan_Brown_November_2015.jpg/220px-Dan_Brown_November_2015.jpg",
+      franchise: false,
+      genre: "Mystery-Thriller",
+      lanugage: "ENGLISH",
+      price: 8.34,
+      rating: 4.2,
+      summary:
+          "การฆาตกรรมในพิพิธภัณฑ์ลูฟวร์ของปารีสและเบาะแสที่คลุมเครือในภาพวาดที่มีชื่อเสียงที่สุดของเลโอนาร์โดดาวินชีนำไปสู่การค้นพบความลึกลับทางศาสนา เป็นเวลา 2,000 ปีแล้วที่สมาคมลับปกป้องข้อมูลอย่างใกล้ชิดว่า - ควรจะเป็นจริงหรือไม่ - สามารถเขย่ารากฐานของศาสนาคริสต์ได้",
+      wideImage: ""),
+
+  Book(
+      title: "Fantastic Beasts And Where To Find Them",
+      cover:
+          "https://upload.wikimedia.org/wikipedia/en/thumb/8/8d/Fantastic_beasts.JPG/220px-Fantastic_beasts.JPG",
+      age: "ALL AGE",
+      author: "J. K. Rowling",
+      authorImg:
+          "https://images.gr-assets.com/authors/1510435123p5/1077326.jpg",
+      franchise: false,
+      genre: "Fantasy",
+      lanugage: "ENGLISH",
+      price: 23.71,
+      rating: 3.5,
+      summary:
+          "ปีนี้คือปี 1926 และ Newt Scamander (Eddie Redmayne) เพิ่งเสร็จสิ้นการเดินทางทั่วโลกเพื่อค้นหาและจัดทำเอกสารเกี่ยวกับสัตว์วิเศษที่ไม่ธรรมดา เมื่อมาถึงนิวยอร์กเพื่อแวะพักช่วงสั้น ๆ เขาอาจจะมาและไปโดยไม่มีอะไรเกิดขึ้นไม่ใช่สำหรับ No-Maj (ชาวอเมริกันสำหรับมักเกิ้ล) ที่ชื่อ Jacob ซึ่งเป็นคดีมหัศจรรย์ที่ใส่ผิดที่และการหลบหนีของสัตว์มหัศจรรย์บางตัวของ Newt ซึ่ง สามารถสะกดปัญหาให้กับทั้งโลกแห่งเวทมนตร์และ No-Maj",
+      wideImage: ""),
+
+  Book(
+      title: "The Alchemist",
+      cover:
+          "https://images-na.ssl-images-amazon.com/images/I/410llGwMZGL._SX328_BO1,204,203,200_.jpg",
+      age: "ALL AGE",
+      author: "Paulo Coelho",
+      authorImg:
+          "https://tpoth-assets.s3.amazonaws.com/uploads/contributor/14/contributor_img/paulo_coelho.jpg",
+      franchise: false,
+      genre: "Quest",
+      lanugage: "ENGLISH",
+      price: 13.69,
+      rating: 4.5,
+      summary:
+          "นักเล่นแร่แปรธาตุติดตามการเดินทางของเด็กเลี้ยงแกะชาวแอนดาลูเซียชื่อซันติอาโก โดยเชื่อว่าความฝันที่เกิดขึ้นซ้ำ ๆ เป็นลางบอกเหตุเขาจึงถามหมอดูโรมานีในเมืองใกล้เคียงเกี่ยวกับความหมาย ผู้หญิงคนนั้นตีความความฝันว่าเป็นคำทำนายที่บอกเด็กผู้ชายว่าเขาจะค้นพบขุมทรัพย์ที่ปิรามิดของอียิปต์",
+      wideImage: ""),
+
+  Book(
+      title: "The Adventures Of Sherlock Holmes",
+      cover:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Adventures_of_sherlock_holmes.jpg/220px-Adventures_of_sherlock_holmes.jpg",
+      age: "ALL AGE",
+      author: "Arthur Conan Doyle",
+      authorImg:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Arthur_Conan_Doyle_by_Walter_Benington%2C_1914.png/220px-Arthur_Conan_Doyle_by_Walter_Benington%2C_1914.png",
+      franchise: false,
+      genre: "Suspense",
+      lanugage: "ENGLISH",
+      price: 15.31,
+      rating: 4.0,
+      summary:
+          "เรื่องราวทั้งหมดใน The Adventures of Sherlock Holmes ได้รับการบอกเล่าในมุมมองบุคคลที่หนึ่งจากมุมมองของดร. วัตสันเช่นเดียวกับในกรณีของ Sherlock Holmes ทั้งสี่เรื่อง รายการ Oxford Dictionary of National Biography สำหรับ Doyle ชี้ให้เห็นว่าเรื่องสั้นที่มีอยู่ใน The Adventures of Sherlock Holmes มักจะชี้ให้เห็นถึงความอยุติธรรมในสังคมเช่น 'การทรยศของกษัตริย์จากนักร้องโอเปร่าการหลอกลวงของพ่อเลี้ยงวอร์ดของเขาในฐานะคู่รักที่สมมติขึ้น โจรชนชั้นสูงที่ใช้ประโยชน์จากโรงรับจำนำที่ล้มเหลวซึ่งเป็นที่ดินกว้างขวางของขอทานในเคนท์' ",
+      wideImage: ""),
+
+  Book(
+      title: "How to build a Billion Dollar App",
+      cover: "https://images.gr-assets.com/books/1420791474l/23658963.jpg",
+      age: "ALL AGE",
+      author: "George Berkowski",
+      authorImg:
+          "https://images-na.ssl-images-amazon.com/images/I/6100CMqLo6L._UY200_.jpg",
+      franchise: false,
+      genre: "Self Analysis",
+      lanugage: "ENGLISH",
+      price: 18.94,
+      rating: 3.5,
+      summary:
+          "Berkowski ดึงเรื่องราวภายในของสมาชิกแอพคลับมูลค่าหลายพันล้านดอลลาร์ซึ่งรวมถึง Instagram, Whatsapp, Snapchat, Candy Crush และ Uber เพื่อให้ข้อมูลทั้งหมดที่คุณต้องการเพื่อสร้างธุรกิจมือถือที่ประสบความสำเร็จอย่างงดงามของคุณเอง เขาแนะนำคุณในแต่ละขั้นตอนตั้งแต่แนวคิดที่เขียนไว้ด้านหลังซองจดหมายไปจนถึงการค้นหาผู้ร่วมก่อตั้งสร้างทีมดึงดูด (และรักษา) ผู้ใช้หลายล้านคนไปจนถึงการเล่นกลกับแรงกดดันในการเป็น CEO ของ a บริษัท พันล้านดอลลาร์ (และยังคงนำหน้าคู่แข่ง)",
+      wideImage: ""),
+
+  Book(
+      title: "ChandraKanta",
+      cover: "https://images-na.ssl-images-amazon.com/images/I/81jtlDN-XbL.jpg",
+      age: "ALL AGE",
+      author: "Devaki Nandan Khatri",
+      authorImg:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Devaki_Nandan_Khatri_Portrait.JPG/220px-Devaki_Nandan_Khatri_Portrait.JPG",
+      franchise: false,
+      genre: "Romantic Fantasy",
+      lanugage: "HINDI",
+      price: 11.99,
+      rating: 4.0,
+      summary:
+          "เรื่องราวเป็นแนวโรแมนติกแฟนตาซีเกี่ยวกับคู่รักสองคนที่อยู่ในอาณาจักรคู่แข่ง: เจ้าหญิงจันทรากันตาแห่งวิเจย์ครห์และเจ้าชายวิเรนดราซิงห์แห่งนอการ์ เรื่องราวค่อยๆคลี่คลายลงเมื่อจันทรกราถูกลักพาตัวและได้รับการช่วยเหลือจากแชปล่า อย่างไรก็ตามพวกเขาติดอยู่ใน Tilism โดยนิสัยใจคอของโชคชะตาเรื่องราว. เรื่องราวเป็นแนวโรแมนติกแฟนตาซีเกี่ยวกับคู่รักสองคนที่อยู่ในอาณาจักรคู่แข่ง: เจ้าหญิงจันทรากันตาแห่งวิเจย์ครห์และเจ้าชายวิเรนดราซิงห์แห่งนอการ์ เรื่องราวค่อยๆคลี่คลายลงเมื่อจันทรกราถูกลักพาตัวและได้รับการช่วยเหลือจากแชปล่า อย่างไรก็ตามพวกเขาติดอยู่ใน Tilism โดยนิสัยใจคอของโชคชะตา",
+      wideImage: ""),
+]);
+
+class BookList {
+  List<Book> books;
+
+  BookList({
+    @required this.books,
+  });
+}
+
+class Book {
+  String title;
+
+  double price;
+  String cover;
+  String author;
+  String genre;
+  String lanugage;
+  String authorImg;
+  bool franchise;
+  String summary;
+  String wideImage;
+  String age;
+  double rating;
+
+  Book({
+    @required this.title,
+    @required this.price,
+    @required this.cover,
+    @required this.author,
+    @required this.age,
+    @required this.authorImg,
+    @required this.franchise,
+    @required this.genre,
+    @required this.lanugage,
+    @required this.summary,
+    @required this.wideImage,
+    @required this.rating,
+  });
+}
